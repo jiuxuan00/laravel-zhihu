d4dac7135b18e06f5be59c80ef3273a9b38af245
jiuxuan <117898271@qq.com>
2018-09-03 18:24:57 +0800

    修改配置路径pp

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -8,10 +8,28 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">面板</div>
                     <div class="panel-body">
-
-                        {{--百度编辑器--}}
-                        @include('vendor.ueditor.assets',['field'=>'body'])
-
+                        <form action="{{url('/questions')}}" method="post">
+                            {{csrf_field()}}
+                            <div class="form-group {{$errors->has('title') ? 'has-error' : ''}}">
+                                <label for="title">标题</label>
+                                <input value="{{old('title')}}" class="form-control" type="text" name="title">
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group {{$errors->has('title') ? 'has-error' : ''}}">
+                                <label for="正文">正文</label>
+                                @include('vendor.ueditor.assets',['field'=>'body'])
+                                @if ($errors->has('body'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('body') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <button type="submit" class="btn btn-primary">确认</button>
+                        </form>
                     </div>
                 </div>
             </div>
